aa7d514d9396487d369d6c28d8a1c97808aee083
Michael-Geuenich <m.geuenich@mail.utoronto.ca>
2024-05-03 13:09:56 -0400

    stop Tiak from complaining

--- pipeline/process-data/create-visium.py
+++ pipeline/process-data/create-visium.py
@@ -1,5 +1,3 @@
-
-#import spatialdata-io as sp
 import scanpy as sc
 import spatialdata_io
 import spatialdata_plot
@@ -14,9 +12,6 @@ vs.table.var['hb'] = vs.table.var_names.str.match('HBA1|HBA2|HBB|HBD|HBE1|HBG1|H
 
 sc.pp.calculate_qc_metrics(vs.table, qc_vars=['mt', 'hb'], percent_top=None, log1p=False, inplace=True)
 
-#sc.pl.spatial(vs.table, color = ["total_counts", "n_genes_by_counts",'pct_counts_mt', 'pct_counts_hb'], spot_size = 300)
-
-
 with plt.rc_context():
     fig, axs = plt.subplots(ncols=4, nrows=1, figsize=(15, 4))
     vs.pl.render_shapes(color="total_counts").pl.show(ax=axs[0], title="total counts", coordinate_systems = 'global')
@@ -27,4 +22,4 @@ with plt.rc_context():
     plt.savefig(snakemake.output['qc_fig'], bbox_inches="tight")
     plt.close()
 
-vs.write(snakemake.output['h5ad'])
+vs.write(snakemake.output['zarr'])
